7ca38c98bcc179819a33d6733d29e9485650fdd0
Marcelo Ortiz de Santana <tentativafc@hotmail.com>
2019-12-22 22:23:54 -0300

    Melhoria do teste.

--- advisor-service/src/test/java/br/com/ortiz/advisor/advices/controllers/AdvicesControllerTest.java
+++ advisor-service/src/test/java/br/com/ortiz/advisor/advices/controllers/AdvicesControllerTest.java
@@ -1,37 +1,28 @@
 package br.com.ortiz.advisor.advices.controllers;
 
 import br.com.ortiz.TestConfig;
+import br.com.ortiz.advisor.advices.model.Advice;
 import br.com.ortiz.advisor.advices.model.repositories.AdviceDao;
 import br.com.ortiz.security.configuration.ConfigProperties;
-import br.com.ortiz.security.model.User;
 import br.com.ortiz.security.services.JwtService;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
 
-import java.util.Optional;
-import java.util.UUID;
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
 
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = TestConfig.class)
-//@WebMvcTest
-//@AutoConfigureMockMvc
 public class AdvicesControllerTest {
 
-//    @Autowired
-//    private MockMvc mvc;
 
     @Autowired
     private ConfigProperties configProperties;
@@ -46,28 +37,15 @@ public class AdvicesControllerTest {
     public JwtService jwtService;
 
     @Test
-    public void testController2() {
-        advicesController.geAllAdvices();
-    }
-
-
-    @Test
     public void testController() throws Exception {
-        User user = new User();
-        user.setEmail("teste@teste.com");
-        user.setFirstName("Teste");
-        user.setLastName("Teste LN");
-        user.setId(UUID.randomUUID());
+        Advice advice = new Advice();
+        advice.setDate(LocalDateTime.now());
+        advice.setIsToBuy(true);
+        advice.setSymbol("PETR4");
+        when(adviceDao.getAll()).thenReturn(Arrays.asList(advice));
+        final List<Advice> advices = advicesController.geAllAdvices();
+        Assert.assertTrue(advices.size() == 1);
 
-        when(jwtService.getTokenFromUser(any(User.class))).thenReturn("123456");
-        when(jwtService.getUserFromToken("123456")).thenReturn(Optional.of(user));
-
-        final String token = jwtService.getTokenFromUser(user);
-
-//        mvc.perform(get("/advices")
-//                .header("Authorization", "Bearer " + token)
-//                .contentType(MediaType.APPLICATION_JSON))
-//                .andExpect(status().isOk());
     }
 
 }
